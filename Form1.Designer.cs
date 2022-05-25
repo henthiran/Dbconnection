9c1275e80bc7afd7f206e6b1bf61a59200fcf814
Henthiran <kenthiran06@gmail.com>
2022-05-25 13:29:17 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace dbonnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRegNo = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtContactNO = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.cmbCourse = new System.Windows.Forms.ComboBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnfirst = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reg No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(441, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(441, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(441, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Course";
+            // 
+            // txtRegNo
+            // 
+            this.txtRegNo.Location = new System.Drawing.Point(119, 28);
+            this.txtRegNo.Name = "txtRegNo";
+            this.txtRegNo.Size = new System.Drawing.Size(217, 20);
+            this.txtRegNo.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(119, 78);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(217, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtContactNO
+            // 
+            this.txtContactNO.Location = new System.Drawing.Point(119, 134);
+            this.txtContactNO.Name = "txtContactNO";
+            this.txtContactNO.Size = new System.Drawing.Size(217, 20);
+            this.txtContactNO.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(508, 28);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(157, 20);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(508, 83);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(48, 17);
+            this.rdMale.TabIndex = 10;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            this.rdMale.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(573, 83);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdFemale.TabIndex = 11;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // cmbCourse
+            // 
+            this.cmbCourse.FormattingEnabled = true;
+            this.cmbCourse.Items.AddRange(new object[] {
+            "ICT",
+            "NCT",
+            "NCE"});
+            this.cmbCourse.Location = new System.Drawing.Point(508, 131);
+            this.cmbCourse.Name = "cmbCourse";
+            this.cmbCourse.Size = new System.Drawing.Size(124, 21);
+            this.cmbCourse.TabIndex = 12;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView.Location = new System.Drawing.Point(42, 180);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(675, 172);
+            this.dataGridView.TabIndex = 13;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            this.dataGridView.DoubleClick += new System.EventHandler(this.dataGridView_DoubleClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(42, 358);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(77, 44);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(226, 358);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 44);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(444, 358);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(84, 44);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(638, 358);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(79, 44);
+            this.btnView.TabIndex = 17;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(226, 425);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(85, 44);
+            this.btnPreview.TabIndex = 19;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(444, 425);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(84, 44);
+            this.btnNext.TabIndex = 20;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(638, 425);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(79, 44);
+            this.btnLast.TabIndex = 21;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnfirst
+            // 
+            this.btnfirst.Location = new System.Drawing.Point(42, 425);
+            this.btnfirst.Name = "btnfirst";
+            this.btnfirst.Size = new System.Drawing.Size(77, 43);
+            this.btnfirst.TabIndex = 22;
+            this.btnfirst.Text = "First";
+            this.btnfirst.UseVisualStyleBackColor = true;
+            this.btnfirst.Click += new System.EventHandler(this.btnfirst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(816, 539);
+            this.Controls.Add(this.btnfirst);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.cmbCourse);
+            this.Controls.Add(this.rdFemale);
+            this.Controls.Add(this.rdMale);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtContactNO);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtRegNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student DB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRegNo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtContactNO;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.ComboBox cmbCourse;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnfirst;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //1 st query to import
+namespace dbonnection
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Secound query connection Sring copy and pate 
+        /// go to server Exploer copy connectionsring and past bellow
+        /// 
+        SqlConnection con = new SqlConnection(@"Data Source=HARDY-PC\SQLEXPRESS;Initial Catalog=db_Students;Integrated Security=True");
+
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+        int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void display()
+        {
+            dataGridView.DataSource = ds;
+            ds.Clear();
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM student";
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "student");
+            dataGridView.DataSource = ds.Tables["student"];
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string gender = "";                                                         
+
+            if (rdMale.Checked)
+            {
+                gender = "male";
+            }
+            if (rdFemale.Checked)
+            {
+                 gender = "female";
+            }
+            // ternary operator
+            //string gender = (rdMale.Checked) ? "male" : "female";
+
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO Student(reg_no,name,contact_no,address,gender,course) VALUES('"+txtRegNo.Text+"','"+txtName.Text+"','"+txtContactNO.Text+"','"+txtAddress.Text+"','"+gender+"','"+cmbCourse.Text+"')";
+            cmd.ExecuteNonQuery();
+            display();
+            con.Close();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM student WHERE reg_no='" + txtRegNo.Text + "'";
+            cmd.ExecuteNonQuery();
+
+            display();
+            con.Close();
+
+
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            display();
+            con.Close();
+
+           
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView_DoubleClick(object sender, EventArgs e)
+        {
+
+
+            string gender = dataGridView.CurrentRow.Cells["gender"].FormattedValue.ToString();
+
+            if (gender.StartsWith("fe"))
+            {
+                rdFemale.Checked = true;
+            }
+            else
+            {
+                rdMale.Checked = true;
+            }
+
+            txtRegNo.Text = dataGridView.CurrentRow.Cells["reg_no"].FormattedValue.ToString();
+            txtName.Text = dataGridView.CurrentRow.Cells["name"].FormattedValue.ToString();
+            txtContactNO.Text = dataGridView.CurrentRow.Cells["contact_no"].FormattedValue.ToString(); 
+            txtAddress.Text = dataGridView.CurrentRow.Cells["address"].FormattedValue.ToString();
+            cmbCourse.Text = dataGridView.CurrentRow.Cells["course"].FormattedValue.ToString();
+            
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string gender = (rdMale.Checked) ? "male" : "female";
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE student SET name='" + txtName.Text + "',contact_no='" + txtContactNO.Text + "',address='" + txtAddress.Text + "',gender='" + gender + "',course='" + cmbCourse.Text + "' WHERE reg_no='" + txtRegNo.Text + "'";
+            cmd.ExecuteNonQuery();
+
+            display();
+            con.Close();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            if (index > 0)
+            {
+                index--;//index = index - 1;
+                txtRegNo.Text = ds.Tables[0].Rows[index]["reg_no"].ToString();
+                txtName.Text = ds.Tables[0].Rows[index]["name"].ToString();
+                txtContactNO.Text = ds.Tables[0].Rows[index]["contact_no"].ToString();
+                txtAddress.Text = ds.Tables[0].Rows[index]["address"].ToString();
+                string gender = ds.Tables[0].Rows[index]["gender"].ToString();
+                if (gender.StartsWith("fe"))
+                {
+                    rdFemale.Checked = true;
+                }
+                else
+                {
+                    rdMale.Checked = true;
+                }
+                cmbCourse.Text = ds.Tables[0].Rows[index]["course"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("You have reached the first row");
+
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (index < ds.Tables[0].Rows.Count - 1)
+            {
+                index++;//index = index + 1;
+                txtRegNo.Text = ds.Tables[0].Rows[index]["reg_no"].ToString();
+                txtName.Text = ds.Tables[0].Rows[index]["name"].ToString();
+                txtContactNO.Text = ds.Tables[0].Rows[index]["contact_no"].ToString();
+                txtAddress.Text = ds.Tables[0].Rows[index]["address"].ToString();
+                string gender = ds.Tables[0].Rows[index]["gender"].ToString();
+                if (gender.StartsWith("fe"))
+                {
+                    rdFemale.Checked = true;
+                }
+                else
+                {
+                    rdMale.Checked = true;
+                }
+                cmbCourse.Text = ds.Tables[0].Rows[index]["course"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("You have reached the last row");
+            }
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            index = ds.Tables[0].Rows.Count - 1;
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                txtRegNo.Text = ds.Tables[0].Rows[index]["reg_no"].ToString();
+                txtName.Text = ds.Tables[0].Rows[index]["name"].ToString();
+                txtContactNO.Text = ds.Tables[0].Rows[index]["contact_no"].ToString();
+                txtAddress.Text = ds.Tables[0].Rows[index]["address"].ToString();
+                string gender = ds.Tables[0].Rows[index]["gender"].ToString();
+                if (gender.StartsWith("fe"))
+                {
+                    rdFemale.Checked = true;
+                }
+                else
+                {
+                    rdMale.Checked = true;
+                }
+                cmbCourse.Text = ds.Tables[0].Rows[index]["course"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Empty Table");
+            }
+        }
+
+        private void btnfirst_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                txtRegNo.Text = ds.Tables[0].Rows[index]["reg_no"].ToString();
+                txtName.Text = ds.Tables[0].Rows[index]["name"].ToString();
+                txtContactNO.Text = ds.Tables[0].Rows[index]["contact_no"].ToString();
+                txtAddress.Text = ds.Tables[0].Rows[index]["address"].ToString();
+                string gender = ds.Tables[0].Rows[index]["gender"].ToString();
+                if (gender.StartsWith("fe"))
+                {
+                    rdFemale.Checked = true;
+                }
+                else
+                {
+                    rdMale.Checked = true;
+                }
+                cmbCourse.Text = ds.Tables[0].Rows[index]["course"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Empty Table");
+            }
+        }
+    }
+    
+    
+}
